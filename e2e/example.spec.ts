e50f85f8a93ad892374d458237bb35f196553951
Joshua Duffy <mail@joshuaduffy.org>
2025-02-27 15:54:44 +0000

    test: add simple check pre/post deployment

---
+++ e2e/example.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Your awesome title/);
+});
+
+test('about link', async ({ page }) => {
+  await page.goto('/');
+
+  // Click the about link.
+  await page.getByRole('link', { name: 'About' }).click();
+
+  // Expects page to have a heading with the name of About.
+  await expect(page.getByRole('heading', { name: 'About' })).toBeVisible();
+});
