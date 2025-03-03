aa4483b804271652eb52d6a9dd5db47811770054
Joshua Duffy <mail@joshuaduffy.org>
2025-03-03 22:40:43 +0000

    feat: update config

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -4,7 +4,7 @@ test('has title', async ({ page }) => {
   await page.goto('/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Your awesome title/);
+  await expect(page).toHaveTitle(/Joshua Duffy/);
 });
 
 test('about link', async ({ page }) => {
